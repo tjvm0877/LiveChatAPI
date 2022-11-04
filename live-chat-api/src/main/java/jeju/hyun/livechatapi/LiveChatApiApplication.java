6212d9188cbf240f526f3a012e00792b54cf2921
Hyun <itsmine096@gmail.com>
2022-11-04 14:55:22 +0900

    프로젝트 생성

---
+++ live-chat-api/src/main/java/jeju/hyun/livechatapi/LiveChatApiApplication.java
@@ -0,0 +1,13 @@
+package jeju.hyun.livechatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveChatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveChatApiApplication.class, args);
+	}
+
+}
